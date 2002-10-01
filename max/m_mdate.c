b741ee4928aa39d58ea76eb3520c248ae4489bf7
sdudley <sdudley>
2002-10-01 17:52:46 +0000

    *** empty log message ***

---
+++ max/m_mdate.c
@@ -0,0 +1,48 @@
+/*
+ * Maximus Version 3.02
+ * Copyright 1989, 2002 by Lanius Corporation.  All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+#pragma off(unreferenced)
+static char rcs_id[]="$Id: M_MDATE.C 1.2 1993/12/05 18:27:59 sjd Exp $";
+#pragma on(unreferenced)
+
+/*# name=Binary-to-ASCII message date routine.
+*/
+
+
+#include <stdio.h>
+#include <io.h>
+#include <fcntl.h>
+#include <string.h>
+#include <stdlib.h>
+#include <mem.h>
+#include "prog.h"
+#include "max_msg.h"
+
+
+char * MsgDate(XMSG *msg,char *datebuf)
+{
+  return (MsgDte((union stamp_combo *)&msg->date_written,datebuf));
+}
+
+
+
+
+
+
+

635649715a389330f6b1699c5caad56efb0dd036
paltas <paltas>
2003-09-22 13:21:29 +0000

    Hmm... Very wierd error.. maybe..

--- squish/s_toss.c
+++ squish/s_toss.c
@@ -18,7 +18,7 @@
  */
 
 #pragma off(unreferenced)
-static char rcs_id[]="$Id: s_toss.c,v 1.3 2003/07/26 00:03:58 rfj Exp $";
+static char rcs_id[]="$Id: s_toss.c,v 1.4 2003/09/10 22:15:48 paltas Exp $";
 #pragma on(unreferenced)
 
 #include <stdio.h>
@@ -699,10 +699,6 @@ static char *products[]=
 
 /* Print out product code for a known product */
 
-#ifdef UNIX
-extern struct ftscprod_ products[];
-#endif
-
 #ifndef UNIX
 #define NUMPROD (sizeof(products)/sizeof(products[0]))
 #endif
@@ -719,6 +715,8 @@ static char * near ProdCode(byte prod)
     return pnum;
   }
 #else
+  extern struct ftscprod_ products[];
+
   if (products[prod].name)
     return products[prod].name;
   else

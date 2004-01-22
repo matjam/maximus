3594210cc1f05f3383e628320dc962bd3280a202
wmcbrine <wmcbrine>
2004-01-22 09:03:43 +0000

    Changed "static char rcs_id" stuff to a comment. Warning supression.

--- max/cppmain.cc
+++ max/cppmain.cc
@@ -17,9 +17,7 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 
-#pragma off(unreferenced)
-static char rcs_id[]="$Id: CPPMAIN.CC 1.3 1995/07/29 07:23:25 sjd Exp $";
-#pragma on(unreferenced)
+/* $Id$ */
 
 #include "prog.h"
 
